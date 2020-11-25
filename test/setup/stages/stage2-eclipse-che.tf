c63bc5e04ae83eb3c115678121e8e19a70c703a6
Sean Sundberg <seansund@us.ibm.com>
2020-11-25 00:21:51 -0600

    Adds workflows and config for module catalog

--- test/setup/stages/stage2-eclipse-che.tf
+++ test/setup/stages/stage2-eclipse-che.tf
@@ -1,9 +1,9 @@
 module "dev_tools_che" {
   source = "github.com/ibm-garage-cloud/terraform-tools-eclipse-che.git"
 
-  cluster_config_file = module.dev_cluster.config_file_path
   cluster_type        = module.dev_cluster.type_code
   ingress_subdomain   = module.dev_cluster.ingress_hostname
+  cluster_config_file = module.dev_cluster.config_file_path
   olm_namespace       = module.dev_software_olm.olm_namespace
   operator_namespace  = module.dev_software_olm.target_namespace
   app_namespace       = module.dev_capture_state.namespace
